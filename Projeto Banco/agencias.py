c5b4045a768585b07441cdb4c904ba86d7d70e1e
CassianoOliveira23 <cassiano.borba@rede.ulbra.br>
2024-05-07 17:36:29 -0300

    herança e polimorfismo

--- Projeto Banco/agencias.py
+++ Projeto Banco/agencias.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Agencia:
     def __init__(self, fone, cnpj, numero):
         self.fone = fone
@@ -28,30 +30,67 @@ class Agencia:
         
         
 # Para a classe AgenciaVirtual existir precisa da classe agencia AgenciaVirtual(Agencia)
-# Conceito de herança, a classe filho herda as informações da classe pai
+# isso é o Conceito de herança, a classe filho herda as informações da classe pai
 class AgenciaVirtual(Agencia):
-    pass
+    #quando definimos um INIT dentro da classe filho ele substitui o da classe pai
+    #adicionar o metodo super para conectar os dois inits e chamar o init principal para herdar as características(métodos e atributos)
+    def __init__(self, site, fone, cnpj):
+        self.site = site
+        super().__init__(fone, cnpj, 1000)
+        self.caixa = 1000000
+        self.caixa_paypal = 0
+    
+    def depositar_paypal(self, valor):
+        self.caixa -= valor
+        self.caixa_paypal += valor
+    
+    def sacar_paypal(self, valor):
+        self.caixa_paypal -= valor
+        self.caixa += valor
+        
+        
 
-
-
-class Agenciacomun(Agencia):
-    pass
+class AgenciaComun(Agencia):
+    def __init__(self, fone, cnpj):
+        super().__init__(fone, cnpj, numero=randint(1001, 9999))
+        self.caixa = 1000000
 
 
 
 class AgenciaPremium(Agencia):
-    pass
+    def __init__(self, fone, cnpj):
+        super().__init__(fone, cnpj, numero=randint(1001, 9999))
+        self.caixa = 100000000
             
-            
-        
-            
-agencia1 = Agencia(666666666, 12345678910, 2304)
+    def adicionar_cliente(self, nome, cpf, patrimonio):
+       if patrimonio >  100000000:
+            super().adicionar_cliente(nome, cpf, patrimonio)
+       else:
+           print("Cliente não possui patrimonio mínimo para entar na Angencia Premium")
+              
 
-agencia_virtual  = AgenciaVirtual(989340681, 236548795412, 3233)
-agencia_virtual.caixa = 15000
-agencia_virtual.verificar_caixa()
 
-agencia_premium = AgenciaPremium(9894477556, 3584681861651, 3333)
-agencia_premium.caixa = 50000
-agencia_premium.verificar_caixa()
+
+
+if __name__ == '__main__':
+       
+    agencia1 = Agencia(666666666, 12345678910, 2304)
+
+    agencia_virtual  = AgenciaVirtual('www.agenciavirtual.com.br', 9898989898, 12345678954325)
+    agencia_virtual.verificar_caixa()
+    agencia_virtual.depositar_paypal(20000)
+    print(agencia_virtual.caixa)
+    print(agencia_virtual.caixa_paypal)
+
+    # Lógica do polomorfismo: O método adicionar_clientes funciona com objetos diferentes, e para cada método ele pode funcionar de forma diferente: nas outras agencias ele adciona qualquer cliente mas na agencia premium ele filtra e só adciona o cliente que cumppre os requisitos
+    # É o mesmo método que é aplicado em classes diferentes
+    agencia_comun = AgenciaComun(9898989898, 987456321021)
+    agencia_comun.adicionar_cliente('Lira Doidao', 1525548485125, 20)
+    print(agencia_comun.clientes)
+
+    agencia_premium = AgenciaPremium(23659874, 659845125151)
+    agencia_premium.adicionar_cliente('Lira', 2356549459659, 9999999999999999  )
+    print(agencia_premium.clientes)
+
+
 
--- Projeto Banco/main.py
+++ Projeto Banco/main.py
@@ -1,20 +1,24 @@
 from ContaBanco import  ContaCorrente, CartaoCredito
+from agencias import AgenciaComun, AgenciaPremium, AgenciaVirtual
 
-#Criando a conta:
-conta_cassiano = ContaCorrente('Cassiano', '032.610.100.45', 2304, 199423)
+# #Criando a conta:
+# conta_cassiano = ContaCorrente('Cassiano', '032.610.100.45', 2304, 199423)
 
-cartao_cassiano = CartaoCredito('Cassiano Oliveira de Borba', conta_cassiano)
-
-
-# SETTER e GETTER
-conta_cassiano.nome = "Cassiano Kovalenko" 
-print(conta_cassiano.nome)
+# cartao_cassiano = CartaoCredito('Cassiano Oliveira de Borba', conta_cassiano)
 
 
-#getter e setter da senha:
-cartao_cassiano.senha = '231994'
-print(cartao_cassiano.senha)
+# # SETTER e GETTER
+# conta_cassiano.nome = "Cassiano Kovalenko" 
+# print(conta_cassiano.nome)
 
 
-print(conta_cassiano.__dict__)
+# #getter e setter da senha:
+# cartao_cassiano.senha = '231994'
+# print(cartao_cassiano.senha)
+
+
+# print(conta_cassiano.__dict__)
+
+agencia_premium_especial = AgenciaPremium(9898989898, 986564879851454)
+print(agencia_premium_especial.caixa)
 
